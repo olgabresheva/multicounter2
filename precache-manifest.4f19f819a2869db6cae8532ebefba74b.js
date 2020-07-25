e143969eb5449da9d7246a0c51db7d0e3709646d
olgab <olgabresheva7@gmail.com>
2020-07-24 23:34:39 -0700

    Updates

---
+++ precache-manifest.4f19f819a2869db6cae8532ebefba74b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f569138586a51e37bed33fd2523c307",
+    "url": "/multicounter2/index.html"
+  },
+  {
+    "revision": "868828b966f7b127c3b7",
+    "url": "/multicounter2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4c2f703c53d7938a845a",
+    "url": "/multicounter2/static/css/main.1d531a09.chunk.css"
+  },
+  {
+    "revision": "868828b966f7b127c3b7",
+    "url": "/multicounter2/static/js/2.c803cc2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/multicounter2/static/js/2.c803cc2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2f703c53d7938a845a",
+    "url": "/multicounter2/static/js/main.6c36a310.chunk.js"
+  },
+  {
+    "revision": "072f1b36f857861673fd",
+    "url": "/multicounter2/static/js/runtime-main.889ec978.js"
+  }
+]);
